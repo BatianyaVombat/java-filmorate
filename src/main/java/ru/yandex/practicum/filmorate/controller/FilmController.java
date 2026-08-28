38b3bc59ac7fce7bee7e3b47b22040008f8ae8e3
BatianyaVombat <alex.strange.game@yandex.ru>
2026-08-28 19:45:12 +0300

    Реализовал функциональность «Фильмы по режиссёрам»

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -9,7 +9,6 @@ import ru.yandex.practicum.filmorate.dto.film.UpdateFilmRequest;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.services.FilmService;
 
-import java.util.Collection;
 import java.util.List;
 
 @RestController
@@ -53,7 +52,13 @@ public class FilmController {
     }
 
     @GetMapping("/popular")
-    public Collection<Film> getPopularFilmList(@RequestParam(defaultValue = "10") Long count) {
+    public List<Film> getPopularFilmList(@RequestParam(defaultValue = "10") Long count) {
         return filmService.getPopularFilmList(count);
+    }
+
+    @GetMapping("/director/{directorId}")
+    public List<FilmResponse> sortedFilmList(@RequestParam(defaultValue = "year") String sortBy,
+                                           @PathVariable Long directorId) {
+        return filmService.getSortedFilms(sortBy,directorId);
     }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/dto/directors/DirectorIdRequest.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.filmorate.dto.directors;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class DirectorIdRequest {
+    private Long id;
+}
